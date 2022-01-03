eed24b692e109955c964cd88af1a310dc1318129
Mochammad Hanif <mochanifz@gmail.com>
2022-01-03 23:28:19 +0700

    Create main.java

---
+++ src/libase/main.java
@@ -0,0 +1,25 @@
+package libase;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        perpustakaan perpus = new perpustakaan();
+        Scanner a = new Scanner(System.in);
+        int nim, jumlah;
+        String namaMHS;
+        System.out.println("==== Selamat Datang di Peminjaman Buku (Perpustakaan) ====");
+        String[] db = perpus.getData();
+        perpus.insertionAsc(db);
+        System.out.println ("\n===========================================================");
+        System.out.println ("INPUT DATA PEMINJAM");
+        System.out.println ("===========================================================");
+        System.out.print   ("Masukkan Nama\t\t\t\t= ");
+        namaMHS = a.nextLine();
+        System.out.print   ("Masukkan Nim\t\t\t\t= ");
+        nim = a.nextInt();
+        System.out.print   ("Masukkan Jumlah Buku\t\t= ");
+        jumlah = a.nextInt();
+        perpus.insert(db,jumlah, nim, namaMHS);
+    }
+}
