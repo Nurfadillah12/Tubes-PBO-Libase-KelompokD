01513ddf6ce107c97c54d1cac4cddcab1d50757d
Mochammad Hanif <mochanifz@gmail.com>
2022-01-04 11:41:02 +0700

    Update main.java

--- src/libase/main.java
+++ src/libase/main.java
@@ -16,10 +16,12 @@ public class main {
         System.out.println ("===========================================================");
         System.out.print   ("Masukkan Nama\t\t\t\t= ");
         namaMHS = a.nextLine();
+        perpus.setNama(namaMHS);
         System.out.print   ("Masukkan Nim\t\t\t\t= ");
         nim = a.nextInt();
+        perpus.setNim(nim);
         System.out.print   ("Masukkan Jumlah Buku\t\t= ");
         jumlah = a.nextInt();
-        perpus.insert(db,jumlah, nim, namaMHS);
+        perpus.insert(db,jumlah);
     }
 }
